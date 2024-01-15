94b192f7264c16401d72d38ead6a8fbf6cf265ab
Eric Tompkins <eric@aptuitiv.com>
2024-01-15 17:14:14 -0500

    Added log level to output build stats

--- scripts/config.js
+++ scripts/config.js
@@ -7,6 +7,7 @@ import eslint from 'esbuild-plugin-eslint';
 const config = {
     bundle: true,
     entryPoints: ['src/index.ts'],
+    logLevel: 'info',
     minify: true,
     outdir: 'dist',
     plugins: [
