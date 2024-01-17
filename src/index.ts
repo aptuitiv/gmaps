a2e9226f45640e32e1384568c73e3f94d161a369
Eric Tompkins <eric@aptuitiv.com>
2024-01-17 12:34:37 -0500

    Removed “self”. Added comments

--- src/index.ts
+++ src/index.ts
@@ -9,26 +9,37 @@ import GlobalObj from './types';
 
 // Set up the global namespace object
 const G: GlobalObj = {
-  latLng,
-  map,
-  marker,
+    latLng,
+    map,
+    marker,
 };
 
+/**
+ * Get the global object for the environment
+ *
+ * globalThis is a global variable in JavaScript that provides a standard way to access the global object
+ * across different environments.
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/globalThis
+ * In a browser, globalThis refers to the window object. In Node.js, it refers to the global object.
+ * Before globalThis was introduced, accessing the global object in a way that works across different
+ * environments could be tricky.
+ * For example, in a browser you could use window, but in Node.js you would need to use global.
+ * globalThis was introduced to solve this problem and provide a consistent way to access the global object.
+ *
+ * @returns {Window | NodeJS.Global | globalThis}
+ */
 function getGlobalObject() {
-  if (typeof globalThis !== 'undefined') {
-    return globalThis;
-  }
-  if (typeof self !== 'undefined') {
-    return self;
-  }
-  if (typeof window !== 'undefined') {
-    return window;
-  }
-  if (typeof global !== 'undefined') {
-    return global;
-  }
+    if (typeof globalThis !== 'undefined') {
+        return globalThis;
+    }
+    if (typeof window !== 'undefined') {
+        return window;
+    }
+    if (typeof global !== 'undefined') {
+        return global;
+    }
 
-  throw new Error('Unable to locate global object.');
+    throw new Error('Unable to locate global object.');
 }
 
 getGlobalObject().G = G;
