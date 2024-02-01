ef643b239d046a844debde42a91e302c48e73baa
Eric Tompkins <eric@aptuitiv.com>
2024-02-01 10:51:51 -0500

    Added support for modifying Point values and cloning

--- src/lib/Point.ts
+++ src/lib/Point.ts
@@ -1,6 +1,9 @@
 /* ===========================================================================
     Represents a set of x and y coordinates.
 
+    The Point value is immutable. Adding, subtracting, or changing the x/y values will return
+    a new Point object.
+
     All methods and options of other classes that take a Point object as a parameter
     also support the point value as an array of [x, y] pairs, or a {x, y} object.
     The following are equivalent:
@@ -15,6 +18,8 @@
     point({x: 34, y: 6});
     point(pointClassInstance);
 =========================================================================== */
+
+/* eslint-disable no-use-before-define */
 
 import { isNumber, isNumberString, isObject } from './helpers';
 
@@ -115,6 +120,91 @@ export class Point {
     }
 
     /**
+     * Returns a new copy of the point
+     *
+     * @returns {Point}
+     */
+    clone(): Point {
+        return new Point(this.x, this.y);
+    }
+
+    /**
+     * Adds the x/y values to this point and returns the result as a new Point object.
+     *
+     * This is the best way to either explicitly add an absolute x/y position, or to combine
+     * two points together. The other point could include negative values.
+     *
+     * @param {PointValue} x The x value, or the Point object, or an array of [x, y] pairs, or a {x, y} object
+     * @param {number|string} [y] The y value
+     * @returns {Point}
+     */
+    add(x: PointValue, y?: number | string): Point {
+        const p2 = point(x, y);
+        return new Point(this.x + p2.getX(), this.y + p2.getY());
+    }
+
+    /**
+     * Returns a copy of the curent point with the x/y values rounded up to the nearest integer.
+     * If the value is already an integer, it will return the same value.
+     *
+     * @returns {Point}
+     */
+    ceil(): Point {
+        return new Point(Math.ceil(this.x), Math.ceil(this.y));
+    }
+
+    /**
+     * Divides the x/y values by a number and returns the result as a new Point object.
+     *
+     * @param {number|string} num The number to divide the x and y values by
+     * @returns {Point}
+     */
+    divide(num: number | string): Point {
+        if (isNumber(num) && num !== 0) {
+            return new Point(this.x / num, this.y / num);
+        }
+        if (isNumberString(num) && Number(num) !== 0) {
+            const n = Number(num);
+            return new Point(this.x / n, this.y / n);
+        }
+        return this.clone();
+    }
+
+    /**
+     * This returns the cartesian distance between this point and the given point.
+     *
+     * @param {PointValue} p The point to compare to
+     * @returns {Number}
+     */
+    distanceTo(p: PointValue): number {
+        const p2 = point(p);
+        const dx = this.x - p2.getX();
+        const dy = this.y - p2.getY();
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    /**
+     * Returns whether the current point is equal to the given point
+     *
+     * @param {PointValue} p The point value to compare
+     * @returns {boolean}
+     */
+    equals(p: PointValue): boolean {
+        const p2 = point(p);
+        return this.x === p2.getX() && this.y === p2.getY();
+    }
+
+    /**
+     * Returns a copy of the curent point with the x/y values rounded down to the nearest integer.
+     * If the value is already an integer, it will return the same value.
+     *
+     * @returns {Point}
+     */
+    floor(): Point {
+        return new Point(Math.floor(this.x), Math.floor(this.y));
+    }
+
+    /**
      * Returns the Google maps point object
      *
      * @link https://developers.google.com/maps/documentation/javascript/reference/coordinates#Point
@@ -141,6 +231,57 @@ export class Point {
     getY(): number {
         return this.y;
     }
+
+    /**
+     * Multiplies the x/y values by a number and returns the result as a new Point object.
+     *
+     * @param {number|string} num The number to multiply the x and y values by
+     * @returns {Point}
+     */
+    multipy(num: number | string): Point {
+        if (isNumber(num) && num !== 0) {
+            return new Point(this.x * num, this.y * num);
+        }
+        if (isNumberString(num) && Number(num) !== 0) {
+            const n = Number(num);
+            return new Point(this.x * n, this.y * n);
+        }
+        return this.clone();
+    }
+
+    /**
+     * Returns a copy of the curent point with the x/y values rounded to the nearest integer.
+     *
+     * @returns {Point}
+     */
+    round(): Point {
+        return new Point(Math.round(this.x), Math.round(this.y));
+    }
+
+    /**
+     * Subtract the x/y values to this point and returns the result as a new Point object.
+     *
+     * The x/y values to subtract should ideally be absolute values to avoid confusion.
+     * While they can include negative numbers, that may return unexpected results.
+     *
+     * @param {PointValue} x The x value, or the Point object, or an array of [x, y] pairs, or a {x, y} object
+     * @param {number|string} [y] The y value
+     * @returns {Point}
+     */
+    subtract(x: PointValue, y?: number | string): Point {
+        const p2 = point(x, y);
+        return new Point(this.x - p2.getX(), this.y - p2.getY());
+    }
+
+    /**
+     * Returns a copy of the curent point with the x/y values changed to the integer part of a number by removing any fractional digits.
+     *
+     * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/trunc
+     * @returns {Point}
+     */
+    trunc(): Point {
+        return new Point(Math.trunc(this.x), Math.trunc(this.y));
+    }
 }
 
 // Possible values for the point in other classes
@@ -149,8 +290,8 @@ export type PointValue = Point | number | number[] | string | string[] | PointOb
 /**
  * Helper function to set up the point object
  *
- * @param {XPoint} x The x value
- * @param {number|string }y The y value
+ * @param {PointValue} x The x value, or the Point object, or an array of [x, y] pairs, or a {x, y} object
+ * @param {number|string} [y] The y value
  * @returns {Point}
  */
 export const point = (x: PointValue, y?: number | string): Point => {
