d460a398eb6425d91285870a7dc38cc1896f0924
Eric Tompkins <eric@aptuitiv.com>
2024-02-06 06:53:23 -0500

    Marker tests

--- site-src/js/marker.js
+++ site-src/js/marker.js
@@ -5,22 +5,46 @@
 
 /* global G */
 
-const map = G.map('map1', {
-    apiKey: apiKey,
-    center: { lat: 36.224, lng: -81.688 },
-});
 
+const map = G.map('map1', { center: { latitude: 48.864716, longitude: 2.3522 } });
+map.display();
 const marker = G.marker({
-    latitude: 40.730610,
-    longitude: -73.935242,
+    latitude: 48.9,
+    longitude: 2.4,
+    map: map,
     title: 'My Marker',
-    tooltipContainer: '#map',
-    tooltipClass: 'my-tooltip'
 });
-marker.addTo(map);
-map.load().then(() => { console.log('1 loaded') });
+// marker.on('click', (e) => {
+//     console.log('Marker Clicked: ', e);
+// });
+G.loader({ apiKey: apiKey }).load();
 
-console.log('map: ', map);
-console.log('isMap: ', map.isMap());
-console.log('isMarker: ', map.isMarker());
+// const loader = G.loader({ apiKey: apiKey, });
+// loader.on('load', () => { console.log('loaded event'); });
+// loader.on('map_loaded', () => { console.log('map loaded event'); });
+// loader.load();
+// const map = G.map('map1', {
+//     apiKey: apiKey,
+//     center: { lat: 36.224, lng: -81.688 },
+// });
+
+// const marker = G.marker({
+//     latitude: 36.224,
+//     longitude: -81.688,
+//     title: 'My Marker',
+//     tooltipContainer: '#map',
+//     tooltipClass: 'my-tooltip'
+// });
+// marker.addTo(map);
+// // marker.on('click', (e) => {
+// //     console.log('Marker Clicked: ', e);
+// // });
+// map.display().then(() => {
+//     console.log('1 loaded')
+//     // marker.addTo(map);
+// });
+
+// console.log('map: ', map);
+// console.log('isMap: ', map.isMap());
+// console.log('isMarker: ', map.isMarker());
 
