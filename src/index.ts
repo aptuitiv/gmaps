b8ab9c5194b7271da50c1c26c81f1861c8bc678e
Eric Tompkins <eric@aptuitiv.com>
2024-04-18 15:40:45 -0400

    Added polyline collection element

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import { map, Map } from './lib/Map';
 import { marker, Marker } from './lib/Marker';
 import { markerCluster, MarkerCluster } from './lib/MarkerCluster';
 import { point, Point } from './lib/Point';
-import { polyline, Polyline } from './lib/Polyline';
+import { polyline, polylineCollection, Polyline } from './lib/Polyline';
 import { popup, Popup } from './lib/Popup';
 import { size, Size } from './lib/Size';
 import { svgSymbol, SvgSymbol } from './lib/SvgSymbol';
@@ -38,6 +38,7 @@ export default {
     point,
     Point,
     polyline,
+    polylineCollection,
     Polyline,
     popup,
     Popup,
--- src/types.ts
+++ src/types.ts
@@ -11,7 +11,7 @@ import { map, Map } from './lib/Map';
 import { marker, Marker } from './lib/Marker';
 import { markerCluster, MarkerCluster } from './lib/MarkerCluster';
 import { point, Point } from './lib/Point';
-import { polyline, Polyline } from './lib/Polyline';
+import { polyline, polylineCollection, Polyline } from './lib/Polyline';
 import { popup, Popup } from './lib/Popup';
 import { size, Size } from './lib/Size';
 import { svgSymbol, SvgSymbol } from './lib/SvgSymbol';
@@ -37,6 +37,7 @@ type GlobalObj = {
     point: typeof point;
     Point: typeof Point;
     polyline: typeof polyline;
+    polylineCollection: typeof polylineCollection;
     Polyline: typeof Polyline;
     popup: typeof popup;
     Popup: typeof Popup;
