37041c2396d6691f840349746b4c08c237fb3925
Eric Tompkins <eric@aptuitiv.com>
2024-09-17 07:59:18 -0400

    Geocode tests

--- site-src/js/geocode.js
+++ site-src/js/geocode.js
@@ -20,7 +20,8 @@ G.loader({ apiKey: apiKey, }).load().then(() => {
         //     country: 'US',
         // },
         // address: '04938',
-        address: '221 B Baker St, London, England',
+        // address: '221 B Baker St, London, England',
+        address: 'no match'
         // bounds: bounds,
         // location: {
         //     // lat: 44.7,
@@ -30,17 +31,30 @@ G.loader({ apiKey: apiKey, }).load().then(() => {
         // },
         // region: 'US',
     });
-    geocoder.run().then((response) => {
+    geocoder.fetch().then((response) => {
         console.log('Response: ', response);
-        console.log('response[0].geometry.location: ', response[0].geometry.location.lat(), response[0].geometry.location.lng());
+        if (response.length > 0) {
+            console.log('response[0].geometry.location: ', response[0].geometry.location.lat(), response[0].geometry.location.lng());
+        }
     })
         .catch((error) => {
             console.error('Error: ', error);
         });
 
-    // G.geocode().run({
+    // G.geocode().fetch({
     //     address: '04938',
     // }).then((response) => {
     //     console.log('Response 2: ', response);
     // });
+
+
+    // (async () => {
+    //     try {
+    //         const result = await geocoder.fetch();
+    //         console.log('Result: ', result);
+    //     } catch (error) {
+    //         console.error('Error: ', error);
+    //     }
+    // })();
+
 });
