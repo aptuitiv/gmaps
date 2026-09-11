c81d891e3e1bd7d8d768da2f17deef156039b214
Eric Tompkins <eric@aptuitiv.com>
2026-09-11 16:48:50 -0400

    Fixed issue where the sources didn’t get sent to Google

--- src/lib/Map/StreetViewControl.ts
+++ src/lib/Map/StreetViewControl.ts
@@ -202,6 +202,8 @@ export class StreetViewControl {
             loader().onLoad(() => {
                 resolve({
                     position: convertControlPosition(this.#position),
+                    // The StreetViewSource values are the same strings that Google's StreetViewSource enum uses
+                    sources: this.#sources as unknown as google.maps.StreetViewSource[],
                 });
             });
         });
